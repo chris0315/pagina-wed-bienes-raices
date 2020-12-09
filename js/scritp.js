63078fd12901f3edafba4e846dce563d76e6785b
chris <cupidodice042014@hotmail.com>
2020-12-08 21:09:29 -0300

    modificando a bienes raices

---
+++ js/scritp.js
@@ -0,0 +1,13 @@
+/*let menu = document.getElementsByClassName("mobile-menu")
+let mobileMenu = () => {
+    console.log("bien")
+    let nav = document.getElementById("navegador")
+    let bt = nav.style.display
+    if (bt == "none") {
+        bt.style.display = "block"
+    } else {
+        bt.style.display = "none"
+    }
+
+}
+menu[0].addEventListener("click", () => mobileMenu())*/
